4897d3f7e1e40971cd83ae2e20e442af70534b7b
m10maeda <mio.maeda.kari@gmail.com>
2023-06-07 23:37:34 +0900

    chore(eslint-config): enable initial disabled rules

--- packages/eslint-config/nest.js
+++ packages/eslint-config/nest.js
@@ -16,10 +16,6 @@ module.exports = {
     node: true,
   },
   rules: {
-    '@typescript-eslint/interface-name-prefix': 'off',
-    '@typescript-eslint/explicit-function-return-type': 'off',
-    '@typescript-eslint/explicit-module-boundary-types': 'off',
-    '@typescript-eslint/no-explicit-any': 'off',
     '@typescript-eslint/no-extraneous-class': [
       'error',
       {
