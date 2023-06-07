e6d94a720704146449247388bc774b11f3d063c9
m10maeda <mio.maeda.kari@gmail.com>
2023-06-07 18:58:45 +0900

    chore(eslint-config): remove unneeded options

--- packages/eslint-config/nest.js
+++ packages/eslint-config/nest.js
@@ -1,11 +1,8 @@
 /** @type {import('eslint/lib/shared/types').ConfigData} */
 module.exports = {
-  parser: '@typescript-eslint/parser',
   parserOptions: {
     project: 'tsconfig.json',
-    sourceType: 'module',
   },
-  plugins: ['@typescript-eslint/eslint-plugin'],
   extends: [
     'plugin:@typescript-eslint/recommended',
     'plugin:prettier/recommended',
